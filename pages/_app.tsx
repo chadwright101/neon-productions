e62e5e03701f66fee73df4c5db574979a83b1215
Chad Wright <justdeejays@gmail.com>
2023-01-07 21:21:54 +0200

    Vercel analytics added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/layout";
@@ -6,6 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 }
